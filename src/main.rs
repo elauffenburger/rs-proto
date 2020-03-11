941076c00ff0cf514264a5a465d8c4330ffd103a
Eric Lauffenburger <elauffenburger@gmail.com>
2020-03-10 23:31:39 -0700

    reog parser modules

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,6 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
-use pest::iterators::{Pair, Pairs};
-use pest::Parser;
-
-mod types;
-mod parser;
-
-use parser::*;
+pub mod parser;
 
 fn main() {}
